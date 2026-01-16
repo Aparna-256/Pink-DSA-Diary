d0a574a8c682be9e16eaa7e2b12fe5a29aeade6d
Aparna-256 <rosary.aparna@gmail.com>
2026-01-16 12:06:19 +0530

    Add solution for Maximum Path Sum in Binary Tree

---
+++ Binary_Tree/Traversals/Postorder_Traversal.cpp
@@ -0,0 +1,22 @@
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+class Solution {
+public:
+    void postorder(TreeNode* root, vector<int>& ans) {
+        if (root == nullptr) return;
+        postorder(root->left, ans);
+        postorder(root->right, ans);
+        ans.push_back(root->val);
+    }
+
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        postorder(root, ans);
+        return ans;
+    }
+};
