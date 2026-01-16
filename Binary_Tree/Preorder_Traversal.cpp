21c86490c7087c2d5addc5b6e4fa74aa264550e5
Aparna-256 <rosary.aparna@gmail.com>
2026-01-16 08:16:35 +0530

    Add preorder traversal of binary tree

--- Binary_Tree/Preorder_Traversal.cpp
+++ Binary_Tree/Preorder_Traversal.cpp
@@ -2,3 +2,29 @@
 // Pattern: DFS (Preorder)
 // Time: O(n), Space: O(h)
 
+/**
+ * Definition for a binary tree node.
+ */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+class Solution {
+public:
+    void preorder(TreeNode* root, vector<int>& ans) {
+        if (root == nullptr) return;
+
+        ans.push_back(root->val);      // Root
+        preorder(root->left, ans);     // Left
+        preorder(root->right, ans);    // Right
+    }
+
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        preorder(root, ans);
+        return ans;
+    }
+};
