478f3c5289118b25bbf2e9eb94a13ec5656b54fc
Aparna Jha <rosary.aparna@gmail.com>
2026-03-30 12:11:45 +0530

    Optimize BST validation method
    Refactored BST check to use O(1) space and O(N) time complexity.

--- Binary_Search_Tree/Check_if_BST_or_BT.cpp
+++ Binary_Search_Tree/Check_if_BST_or_BT.cpp
@@ -1,3 +1,6 @@
+//space : O(N)
+//time  : O(N)
+
 class Solution {
 public:
     void inorder(TreeNode* root, vector<int>& v) {
@@ -20,3 +23,27 @@ public:
         return true;
     }
 };
+
+------------------------------------------------------------
+
+//SPACE : O(1)
+//TIME  : O(N)
+
+class Solution {
+public:
+    bool solve(TreeNode* root, long minVal, long maxVal) {
+        if (root == NULL) return true;
+
+        if (root->val <= minVal || root->val >= maxVal) {
+            return false;
+        }
+
+        return solve(root->left, minVal, root->val) &&
+               solve(root->right, root->val, maxVal);
+    }
+
+    bool isBST(TreeNode* root) {
+        return solve(root, LONG_MIN, LONG_MAX);
+    }
+};
+
