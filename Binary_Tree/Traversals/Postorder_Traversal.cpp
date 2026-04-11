c174ca4f6006e5ed66b257db242d4aebbd1d7043
Aparna Jha <rosary.aparna@gmail.com>
2026-04-11 17:50:35 +0530

    Implement postorder traversal using two approaches
    Added two approaches for postorder traversal: 2-stack and 1-stack methods.

--- Binary_Tree/Traversals/Postorder_Traversal.cpp
+++ Binary_Tree/Traversals/Postorder_Traversal.cpp
@@ -1,3 +1,5 @@
+RECUSSION APPROACH
+
 struct TreeNode {
     int val;
     TreeNode *left;
@@ -20,3 +22,61 @@ public:
         return ans;
     }
 };
+
+-------------------------------------------------------------------------------
+
+2-STACK APPROACH
+
+vector<int> postorderTraversal(TreeNode* root) {
+    vector<int> res;
+    if (!root) return res;
+
+    stack<TreeNode*> st1, st2;
+    st1.push(root);
+
+    while (!st1.empty()) {
+        TreeNode* node = st1.top();
+        st1.pop();
+        st2.push(node);
+
+        if (node->left) st1.push(node->left);
+        if (node->right) st1.push(node->right);
+    }
+
+    while (!st2.empty()) {
+        res.push_back(st2.top()->val);
+        st2.pop();
+    }
+
+    return res;
+}
+
+---------------------------------------------------------------------------------------
+
+1-STACK APPROACH
+
+vector<int> postorderTraversal(TreeNode* root) {
+    vector<int> res;
+    stack<TreeNode*> st;
+    TreeNode* lastVisited = NULL;
+
+    while (root || !st.empty()) {
+        if (root) {
+            st.push(root);
+            root = root->left;
+        } else {
+            TreeNode* node = st.top();
+
+            // if right exists and not processed yet
+            if (node->right && lastVisited != node->right) {
+                root = node->right;
+            } else {
+                res.push_back(node->val);
+                lastVisited = node;
+                st.pop();
+            }
+        }
+    }
+
+    return res;
+}
