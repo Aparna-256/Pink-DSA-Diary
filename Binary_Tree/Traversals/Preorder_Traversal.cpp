26bf197db30a230a7e243b3c28c2986689a74dc1
Aparna Jha <rosary.aparna@gmail.com>
2026-04-11 17:21:18 +0530

    Implement iterative preorder traversal method
    Added iterative approach for preorder traversal of a binary tree.

--- Binary_Tree/Traversals/Preorder_Traversal.cpp
+++ Binary_Tree/Traversals/Preorder_Traversal.cpp
@@ -28,3 +28,28 @@ public:
         return ans;
     }
 };
+
+------------------------------------------------------------------------------------
+ITERATIVE APPROACH
+
+    
+vector<int> preorderTraversal(TreeNode* root) {
+    vector<int> res;
+    if (!root) return res;
+
+    stack<TreeNode*> st;
+    st.push(root);
+
+    while (!st.empty()) {
+        TreeNode* node = st.top();
+        st.pop();
+
+        res.push_back(node->val);
+
+        // push right first so left is processed first
+        if (node->right) st.push(node->right);
+        if (node->left) st.push(node->left);
+    }
+
+    return res;
+}
