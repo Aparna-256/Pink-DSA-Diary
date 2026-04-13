f82a2f8ecb4581ee3d37fee073f224e572158840
Aparna Jha <rosary.aparna@gmail.com>
2026-04-13 20:44:19 +0530

    Refactor level order traversal to use result vector

--- Binary_Tree/Traversals/Levelorder_Traversal.cpp
+++ Binary_Tree/Traversals/Levelorder_Traversal.cpp
@@ -1,19 +1,9 @@
-#include <queue>
-#include <vector>
-using namespace std;
-
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-};
-
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
-        vector<vector<int>> ans;
-        if (!root) return ans;
+        vector<vector<int>> result;
+
+        if (!root) return result;
 
         queue<TreeNode*> q;
         q.push(root);
@@ -27,12 +17,14 @@ public:
                 q.pop();
 
                 level.push_back(node->val);
+
                 if (node->left) q.push(node->left);
                 if (node->right) q.push(node->right);
             }
 
-            ans.push_back(level);
+            result.push_back(level);
         }
-        return ans;
+
+        return result;
     }
 };
